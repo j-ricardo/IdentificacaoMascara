06fda022df0b127e315c08600a297ea0d0d0027a
Ricardo <ricardo.junior_@outlook.com>
2021-06-30 15:20:02 -0300

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbFoto = new System.Windows.Forms.PictureBox();
+            this.bDetectar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbCamera = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbFoto)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbFoto
+            // 
+            this.pbFoto.Location = new System.Drawing.Point(12, 58);
+            this.pbFoto.Name = "pbFoto";
+            this.pbFoto.Size = new System.Drawing.Size(831, 504);
+            this.pbFoto.TabIndex = 0;
+            this.pbFoto.TabStop = false;
+            // 
+            // bDetectar
+            // 
+            this.bDetectar.Location = new System.Drawing.Point(862, 13);
+            this.bDetectar.Name = "bDetectar";
+            this.bDetectar.Size = new System.Drawing.Size(105, 50);
+            this.bDetectar.TabIndex = 1;
+            this.bDetectar.Text = "Detectar";
+            this.bDetectar.UseVisualStyleBackColor = true;
+            this.bDetectar.Click += new System.EventHandler(this.BDetectar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Câmera:";
+            // 
+            // cbCamera
+            // 
+            this.cbCamera.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbCamera.FormattingEnabled = true;
+            this.cbCamera.Location = new System.Drawing.Point(106, 10);
+            this.cbCamera.Name = "cbCamera";
+            this.cbCamera.Size = new System.Drawing.Size(241, 32);
+            this.cbCamera.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(979, 574);
+            this.Controls.Add(this.cbCamera);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bDetectar);
+            this.Controls.Add(this.pbFoto);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbFoto)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbFoto;
+        private System.Windows.Forms.Button bDetectar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbCamera;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,138 @@
+﻿using AForge.Video;
+using AForge.Video.DirectShow;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private FilterInfoCollection filterInfoCollection;
+        private VideoCaptureDevice VideoCaptureDevice;
+
+        private static readonly CascadeClassifier Face_Cascade1 = new CascadeClassifier("haarcascade_profileface.xml");
+        private static readonly CascadeClassifier Face_Cascade2 = new CascadeClassifier("haarcascade_upperbody.xml");
+        private static readonly CascadeClassifier Face_Cascade3 = new CascadeClassifier("haarcascade_frontalface_alt2.xml");
+        private static readonly CascadeClassifier Face_Cascade4 = new CascadeClassifier("haarcascade_frontalface_default.xml");
+        private static readonly CascadeClassifier Face_Cascade5 = new CascadeClassifier("haarcascade_frontalface_alt.xml");
+        private static readonly CascadeClassifier Mouth_Cascade = new CascadeClassifier("Mouth.xml");
+        private static readonly CascadeClassifier Nose_Cascade = new CascadeClassifier("Nose.xml");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            filterInfoCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach(FilterInfo filterInfo in filterInfoCollection)            
+                cbCamera.Items.Add(filterInfo.Name);
+
+            cbCamera.SelectedIndex = 0;
+            VideoCaptureDevice = new VideoCaptureDevice();
+        }
+
+        private void BDetectar_Click(object sender, EventArgs e)
+        {
+            if (!VideoCaptureDevice.IsRunning)
+            {
+                VideoCaptureDevice = new VideoCaptureDevice(filterInfoCollection[cbCamera.SelectedIndex].MonikerString);
+                VideoCaptureDevice.NewFrame += VideoCaptureDevice_NewFrame;
+                VideoCaptureDevice.Start();
+            }
+        }
+
+        private void VideoCaptureDevice_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            TratarImagem((Bitmap)eventArgs.Frame.Clone());
+        }
+
+        private void TratarImagem(Bitmap bitmap)
+        {
+            try
+            {
+                bitmap.Save("img.bmp");
+                var gray = new Image<Gray, byte>("img.bmp");
+                gray.Save("rosto.bmp");
+
+                var correcao = new Image<Gray, byte>("rosto.bmp");
+                //var black_and_white = new Image<Bgr, byte>("img.bmp");
+
+                //var faces1 = Face_Cascade1.DetectMultiScale(gray, 3);
+                //var faces2 = Face_Cascade2.DetectMultiScale(gray, 3);
+                //var faces3 = Face_Cascade3.DetectMultiScale(gray, 3);
+                var faces4 = Face_Cascade4.DetectMultiScale(gray, 8);
+                //var faces5 = Face_Cascade5.DetectMultiScale(gray, 6, 3);
+
+                //if (faces1.Length > 0)
+                //    TratarFace(bitmap, faces1);
+
+                //if (faces2.Length > 0)
+                //    TratarFace(bitmap, faces2);
+
+                //if (faces3.Length > 0)
+                //    TratarFace(bitmap, faces3);
+
+                if (faces4.Length > 0)
+                    TratarFace(bitmap, faces4);
+
+                //if (faces5.Length > 0)
+                //    TratarFace(bitmap, faces5);
+            }
+
+            finally { pbFoto.Image = bitmap; }
+        }
+
+        private void TratarFace(Bitmap bitmap, Rectangle[] faces)
+        {
+            var weared_mask = "Está usando mascara";
+            var not_weared_mask = "Não esta usando mascara";
+
+            foreach (var retangulo in faces)
+            {
+                var rosto = CropImage(bitmap, retangulo);
+                rosto.Save("Rosto.bmp");
+                var NovoGray = new Image<Gray, byte>("Rosto.bmp");
+                var mouth_rects = Mouth_Cascade.DetectMultiScale(NovoGray, 5);
+                //var nose_rects = Nose_Cascade.DetectMultiScale(NovoGray, 2, 1);
+                if (mouth_rects.Length == 0)
+                {
+                    var greenBrush = new SolidBrush(Color.Green);
+                    var graphics = Graphics.FromImage(bitmap);
+                    graphics.DrawString(weared_mask, new Font("Arial", 10), greenBrush, retangulo.Location);
+                    graphics.DrawRectangle(new Pen(Color.Green, 2), retangulo.X, retangulo.Y, retangulo.Width, retangulo.Height);
+                }
+
+                else
+                {
+                    foreach (var mouth in mouth_rects)
+                    {
+                        var redBrush = new SolidBrush(Color.Red);
+                        var graphics = Graphics.FromImage(bitmap);
+                        graphics.DrawString(not_weared_mask, new Font("Arial", 10), redBrush, retangulo.Location);
+                        graphics.DrawRectangle(new Pen(Color.Red, 2), retangulo.X, retangulo.Y, retangulo.Width, retangulo.Height);
+                    }
+                }
+
+                File.Delete("Rosto.bmp");
+            }
+        }
+
+        private Bitmap CropImage(Image img, Rectangle cropArea)
+        {
+            var bmpImage = new Bitmap(img);
+            return bmpImage.Clone(cropArea, bmpImage.PixelFormat);
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (VideoCaptureDevice.IsRunning)
+                VideoCaptureDevice.Stop();
+        }
+    }
+}
